ca86d20e39016b375e2c81effe949eccfcad1441
Electrovirus <E.electrovirus@gmail.com>
2017-02-10 21:55:07 +0330

    Ruby packages

---
+++ ruby.sh
@@ -0,0 +1,15 @@
+config() {
+gem install telegram-bot-ruby
+gem install colorize
+gem install json
+}
+
+if [ "$1" = "config" ]; then
+  config
+ fi
+
+   echo -e "\033[38;5;208m"
+   echo -e "     > Developed by [Electrovirus]      "
+   echo -e "                                              \033[0;00m"
+   echo -e "\e[36m"
+fi
