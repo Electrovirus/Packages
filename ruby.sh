8b2626851205476772dada9c590c038fb8955852
Electrovirus <E.electrovirus@gmail.com>
2017-02-15 20:30:55 +0330

    Ruby packages

--- ruby.sh
+++ ruby.sh
@@ -8,8 +8,15 @@ if [ "$1" = "ruby2.4" ]; then
   ruby2.4
  fi
 
+
    echo -e "\033[38;5;208m"
-   echo -e "     > Developed by [Electrovirus]      "
+   echo -e "     > Developed by [Electrovirus]  "
+   sleep 1
+   echo -e "  _______     __  _____           "         
+   echo -e " | ____\ \   / / |_   _|__  __ _ _ __ ___  "
+   echo -e " |  _|  \ \ / /    | |/ _ \/ _` | '_ ` _ \ "
+   echo -e " | |___  \ V /     | |  __/ (_| | | | | | |"
+   echo -e " |_____|  \_/      |_|\___|\__,_|_| |_| |_|"
    echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
 fi
